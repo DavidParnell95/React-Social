163868102f264f606113337575dbe120969097d0
DavidParnell95 <55593664+DavidParnell95@users.noreply.github.com>
2020-06-23 13:02:01 +0100

    Login function and validation implemented

--- reactsocial-functions/functions/index.js
+++ reactsocial-functions/functions/index.js
@@ -2,6 +2,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const firebase = require('firebase');
 const e = require('express');
+const { auth } = require('firebase-admin');
 
 const app = require('express')();
 admin.initializeApp();
@@ -43,6 +44,15 @@ app.get('/posts', (req,res) =>{
         .catch(err => console.log(err))//Log errors to console
 })
 
+const isEmpty = (string) => {
+    if(string.trim() === '')
+    {
+        return true;
+    }
+
+    else{ return false }
+}
+
 //Create new post with data from request object
 app.post('/post',(req,res) => {
    
@@ -60,8 +70,22 @@ app.post('/post',(req,res) => {
     .catch(err => {
         res.statusCode(500).json({error: 'something messed up :('});
         console.log(err);
-        })
     })
+})
+
+//checks if valid email
+const isEmail = (email) => {
+    const regEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if(email.match(regEX))
+    {
+        return true;
+    }
+
+    else{
+        return false;
+    }
+}
 
 //Sign Up Route
 app.post('/signup', (req,res) => {
@@ -72,14 +96,48 @@ app.post('/signup', (req,res) => {
         handle: req.body.handle,
     };
 
-    //TODO: validate
+    let errors = {};
+
+    //Email Checking 
+    //Check if email entered
+    if(isEmpty(newUser.email))
+    {
+        errors.email = "Mustn't be empty"
+    }
+
+    //Check if email is a valid email
+    else if(!isEmail(newUser.email))
+    {
+        errors.email = "Must be a valid email address"
+    }
+
+    //Password Checking
+    //Check if password entered
+    if(isEmpty(newUser.password)){ 
+        errors.password = "Mustn't be Empty"
+    }
+
+    //Check if password and confirmPassword match
+    if(newUser.password !== newUser.confirmPassord)
+    {
+        errors.confirmPassord = "Passwords do not match"
+    }
+
+    //If errors
+    if(Object.keys(errors).length > 0)
+    {
+        return res.status(400).json(errors);
+    }
+
+    //Check if the user already exists
     db.doc(`/users/${newUser.handle}`).get()
     .then(doc =>{
+        let token, userId;
 
         //check if user document already exists
         if(doc.exists)
         {
-            return res.status(400).json({handle: 'user already exists'})            
+            return res.status(400).json({handle: 'username already exists'})            
         }//end if exists
 
         else{
@@ -88,18 +146,87 @@ app.post('/signup', (req,res) => {
             .then(data =>{
                 return data.user.getIdToken();
             })
-            .then(token => {
-                return res.status(201).json({token});
+            .then(idToken => {
+                token = idToken;
+                userId = data.user.userId;
+                
+                //Prep user infor for document
+                const userCred = {
+                    handle: newUser.handle,
+                    email: newUser.email,
+                    createdAt: new Date().toISOString(),
+                    userId
+                }
+
+                return db.doc(`/users/${newUser.handle}`).set(userCred);
+            }).then(() => {
+                return res.status(201).json({ token });
             })
+
+
             .catch(err => {
                 console.error(err);
-                return res.status(500).json({error: err.code});
+
+                //If email exists error 
+                if(err.code === 'auth/email-aleardy-in-user')
+                {
+                    return res.status(500).json({ email: 'Email is already in user'});
+                }
+
+                //Everything else
+                else{
+                    return res.status(500).json({error: err.code});
+                }
+                
             })
 
         }//End else
     })
 
+});
+
+//Login Route
+app.post('/login', (req,res) => {
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    let errors = {};
+
+    //check email
+    if(isEmpty(user.email)) {
+        errors.email = "Mustn't be empty"
+    }
+
+    //Check password
+    if(isEmpty(user.password)) {
+        errors.password = "Mustn't be empty"
+    }
+
+    //check if errors 
+    if(Object.keys(errors).length > 0 )
+    {
+        return res.status(400).json(errors);
+    }
+    
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(data => {
+            return data.getIdToken();
+        }).then(token => {
+            return res.json({ token })
+        }).catch(err => {
+            if(err.code === 'auth/wrong-password')
+            {
+                return res.status(403).json({general: 'Wrong credentials, try again'})
+            }
+
+            else{
+                return res.status(500).json({ error: err.code })
+            }
+            
+        })
 })
 
 
-    exports.api = functions.region('europe-west1').https.onRequest(app);
+exports.api = functions.region('europe-west1').https.onRequest(app);
