7e00b21a3ac14c7dfd9316404dfecd5020a4d286
DavidParnell95 <55593664+DavidParnell95@users.noreply.github.com>
2020-06-25 15:52:36 +0100

    image upload half implemented

--- reactsocial-functions/functions/routes/posts.js
+++ reactsocial-functions/functions/routes/posts.js
@@ -1,4 +1,9 @@
-const {db} = require('../util/admin');
+const { admin , db } = require('../util/admin');
+const BusBoy = require('busboy');
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+const config = require('../util/config');
 
 exports.getAllPosts =  (req,res) =>{
     db.collection('posts')
@@ -35,5 +40,47 @@ exports.createNewPost = (req,res) => {
     .catch(err => {
         res.statusCode(500).json({error: 'something messed up :('});
         console.log(err);
+    })
+}
+
+let imageFilename;
+let imageToBeUploaded = {};
+
+exports.imageUpload = (req,res) => {
+    const busboy = new BusBoy({headers: req.headers});
+    
+    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+        //Split file name on . 
+        const extension = filename.split('.')[filename.split('.').length -1];
+        imageFilename = `${Math.round(Math.random()*100000000)}.${extension}`;
+
+        const filepath = path.join(os.tmpdir(), imageFilename);
+        imageToBeUploaded = { filepath, mimetype}
+        
+        console.log("fieldname: " + fieldname + "\n filename: " + filename + "\n mimetype: " +mimetype)
+
+        //Creates the file
+        file.pipe(fs.createWriteStream(filepath))
+    });
+
+    //uploads the file 
+    busboy.on('finish', () => {
+        admin.storage().bucket().upload(imageToBeUploaded, filepath), {
+            resumable: false,
+            metadata: {
+                metadata: {
+                    contenType: imageToBeUploaded.mimetype
+                }
+            }
+        }
+    }).then(() =>{
+        const imageURL = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFilename}?alt=media`
+        return db.doc(`/users/${req.user.handle}`).update({imageURL})
+    }).then(() =>{
+        return res.json({message: "image uploaded"})
+    })
+    .catch(err =>{
+        console.error(err);
+        return res.status(500).json({error: err.code});
     })
 }
